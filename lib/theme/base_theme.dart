91248fb32cd3ee7b7ee92a4e1e6dc4fcfd7f53f1
Sakyra-team <sakyra568@gmail.com>
2026-02-26 19:11:07 +0300

    Text theme dark

--- lib/theme/base_theme.dart
+++ lib/theme/base_theme.dart
@@ -14,7 +14,7 @@ const TextTheme darkTextTheme = TextTheme(
     fontFamily: "Montserrat",
   ),
   titleSmall: TextStyle(
-    fontSize: 18,
+    fontSize: 18, 
     fontWeight: FontWeight.w500,
     color: Colors.white,
     fontFamily: "Montserrat",
